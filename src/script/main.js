d9b441b5c91ff7894bda61e1d0126b77503ab629
zauvik <zauvik.m@students.amikom.ac.id>
2020-04-25 00:52:06 +0700

    ADDED : submission finished, please learn the flow

--- src/script/main.js
+++ src/script/main.js
@@ -1,75 +1,50 @@
 const main = () => {
-    const getZodiak = async (zodiak) => {
-        console.log(zodiak)
+    const inputNama = document.querySelector("#inputNama");
+    const buttonTampil = document.querySelector("#buttonTampil");
+    const app = document.querySelector("#app");
+    
+    buttonTampil.addEventListener("click", async function () {
+        const negara = inputNama.value;
+
+        //perform fetch data
+        const data = await getData(negara)
+
+        //perform render to html
+        render(data)
+    });
+
+   
+    //fetch data
+    const getData = async (negara) => {
+        console.log(negara)
+        let result = null
         const options = {
             method: "GET",
             headers: {
                 "Content-Type": "application/json",
             },
         }
-        const url = 'https://script.google.com/macros/exec?service=AKfycbw7gKzP-WYV2F5mc9RaR7yE3Ve1yN91Tjs91hp_jHSE02dSv9w&nama=' + zodiak.nama + '&tanggal=' + zodiak.lahir
-        fetch(url, options)
+        const url = 'https://corona.lmao.ninja/v2/countries/'+negara
+        await fetch(url, options)
             .then(r => r.json())
-            .then(data => console.log("hasil data : " + data))
+            .then(r => result = r)
             .catch(e => console.log(e))
+        return result 
     }
 
-    const inputNama = document.querySelector("#inputNama");
-    const inputLahir = document.querySelector("#inputLahir");
-    const buttonTampil = document.querySelector("#buttonTampil");
-    buttonTampil.addEventListener("click", function () {
-        const data = {
-            nama: inputNama.value,
-            lahir: inputLahir.value
-        };
-        console.log("nama : " + data.nama)
-        console.log("lahir : " + data.lahir)
-        getZodiak(data)
-    });
+    //render
+    const render = (data)=>{
+        const html = `
+        <h1>Negara : ${data.country}</h1>
+        <div class="alert alert-warning">Total Kasus : ${data.cases} | Kasus Hari Ini : ${data.todayCases}</div>
+        <div class="alert alert-primary">Total dirawat : ${data.active}</div>
+        <div class="alert alert-success">Total Sembuh : ${data.recovered}</div>
+        <div class="alert alert-danger">Total Meninggal : ${data.deaths} | Meninggal Hari Ini : ${data.todayDeaths}</div>
+        
+        `
 
-    // const removeZodiak = (nama) => {
-    //     fetch(`https://script.google.com/macros/exec?service=AKfycbw7gKzP-WYV2F5mc9RaR7yE3Ve1yN91Tjs91hp_jHSE02dSv9w&nama=&tanggal/delete/${nama}`, {
-    //         method: "DELETE",
-    //         headers: {
-    //         }
-    //     })
-    //     .then(response => {
-    //         return response.json();
-    //     })
-    //     .then(responseJson => {
-    //         showResponseMessage(responseJson.message);
-    //         getZodiak();      
-    //     })
-    //     .catch(error => {
-    //         showResponseMessage(error);
-    //     })
-    // };
-
-    // const renderAllZodiak = (data) => {
-    //     const hasilZodiakElement = document.querySelector("#zodiakHasil");
-    //     hasilZodiakElement.innerHTML = "";
-
-    //     data.forEach(data => {
-    //         hasilZodiakElement.innerHTML += `
-    //             <div class="col-lg-4 col-md-6 col-sm-12" style="margin-top: 12px;">
-    //                 <div class="card">
-    //                     <div class="card-body">
-    //                         <h5>Hello ${data.nama} ${data.lahir} zodiak kamu adalah ${data.zodiak}</h5>
-    //                         <button type="button" class="btn btn-danger button-delete">Hapus</button>
-    //                     </div>
-    //                 </div>
-    //             </div>
-    //         `;
-    //     });
-
-    //     const buttons = document.querySelectorAll(".button-delete");
-    //     buttons.forEach(button => {
-    //         button.addEventListener("click", event => {
-    //             const nama = event.target.nama;
-    //             removeZodiak(nama);
-    //         })
-    //     })
-    // };
+        app.innerHTML = html
+    }
 
 
 }
